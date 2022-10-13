da066120ff51d33660b0d27ead0668417ced26aa
Denislav <deni_9000_1990@abv.bg>
2022-10-13 20:16:26 +0200

    test icp files-3

--- Kalmandeni/Results_robo_scans/icp_git/basicICP.py
+++ Kalmandeni/Results_robo_scans/icp_git/basicICP.py
@@ -1,45 +1,7 @@
-# -*- coding: utf-8 -*-
-# basicICP.py
-
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# * Redistributions of source code must retain the above copyright
-#   notice, this list of conditions and the following disclaimer.
-# * Redistributions in binary form must reproduce the above copyright
-#   notice, this list of conditions and the following disclaimer in the
-#   documentation and/or other materials provided with the distribution.
-# * Neither the name of the copyright holders nor the names of any
-#   contributors may be used to endorse or promote products derived
-#   from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-"""
-Created on Mon Jan 23 18:08:58 2017
-
-@author: agnivsen
-
-A basic Iterative Closes Point matching betwwen two set of point cloud, 
-                        or one point cloud and one point + normal pair.
-                        
-Basic point to plane matching has been done using a Least squares approach and a Gauss-Newton approach
-Point to point matching has been done using Gauss-Newton only
-"""
 
 import numpy as np
 import re
-
 from numpy.linalg import pinv
-
 import transformations as transform
 
 
@@ -84,9 +46,9 @@ def read_file_deformed(file_path):
 def icp_point_to_plane(source_points, dest_points,loop):
     """
     Point to plane matching using least squares
-    
     source_points:  nx3 matrix of n 3D points
-    dest_points: nx6 matrix of n 3D points + 3 normal vectors, which have been obtained by some rigid deformation of 'source_points'
+    dest_points: nx6 matrix of n 3D points + 3 normal vectors, which have been obtained by some
+     rigid deformation of 'source_points'
     """
     
     A = []
@@ -144,7 +106,7 @@ def icp_point_to_plane(source_points, dest_points,loop):
         p = np.dot(R,ss)
         source_transformed.append(p)
     source_points = np.array(source_transformed)
-    print(source_points)
+    #print(source_points)
     loop = loop + 1
     if(loop < 1):   #although this should converge in one step (which it does), you might want to reiterate over and over, just for the fun of it!
         icp_point_to_plane(source_points,dest_points,loop)
@@ -156,10 +118,16 @@ def icp_point_to_point_lm(source_points, dest_points,initial,loop):
     Point to point matching using Gauss-Newton
     
     source_points:  nx3 matrix of n 3D points
-    dest_points: nx3 matrix of n 3D points, which have been obtained by some rigid deformation of 'source_points'
-    initial: 1x6 matrix, denoting alpha, beta, gamma (the Euler angles for rotation and tx, ty, tz (the translation along three axis). 
-                this is the initial estimate of the transformation between 'source_points' and 'dest_points'
-    loop: start with zero, to keep track of the number of times it loops, just a very crude way to control the recursion            
+
+    dest_points: nx3 matrix of n 3D points, which have been obtained by some rigid deformation
+    of 'source_points'
+
+    initial: 1x6 matrix, denoting alpha, beta, gamma (the Euler angles for rotation and tx, ty, tz
+    (the translation along three axis). this is the initial estimate of the transformation
+    between 'source_points' and 'dest_points'
+
+    loop: start with zero, to keep track of the number of times it loops, just a very crude way to
+     control the recursion
                 
     """
     
@@ -196,13 +164,17 @@ def icp_point_to_point_lm(source_points, dest_points,initial,loop):
         _residual = (a4*a4/4)+(a5*a5/4)+(a6*a6/4)
         
         _J = np.array([a1, a2, a3, a4, a5, a6])
-        _e = np.array([_residual])
+        #print(_J)
+        _e = np.array([_residual]) # error
+
         
         J.append(_J)
         e.append(_e)
         
     jacobian = np.array(J)
+    #print(jacobian)
     residual = np.array(e)
+    print(residual)
     
     update = -np.dot(np.dot(np.linalg.pinv(np.dot(np.transpose(jacobian),jacobian)),np.transpose(jacobian)),residual)
     #print(update)
@@ -212,11 +184,11 @@ def icp_point_to_point_lm(source_points, dest_points,initial,loop):
     #print(initial)
 
     
-    print( np.transpose(initial))
+    #print( np.transpose(initial))
     
     loop = loop + 1
     
-    if(loop < 5):  # here lies the control variable, control the number of iteration from here
+    if(loop < 2):  # here lies the control variable, control the number of iteration from here
     
         icp_point_to_point_lm(source_points,dest_points,initial, loop)
         
@@ -292,24 +264,7 @@ def icp_point_to_plane_lm(source_points, dest_points,initial,loop):
 
 
 
-# fileOriginal = '/home/asengupt/Documents/icp/data/original.xyz'
-# deformed = '/home/asengupt/Documents/icp/data/deformed.xyz'
-#
-# source_points = read_file_original(fileOriginal)
-# dest_points_et_normal = read_file_deformed(deformed)
-#
-# initial = np.array([[0.01], [0.05], [0.01], [0.001], [0.001], [0.001]])
 
-# **********************************************************************
-# Uncomment one of the following lines to trigger the respective module:
-# **********************************************************************
-
-
-#icp_point_to_plane(source_points,dest_points_et_normal,0)
-
-#icp_point_to_point_lm(source_points,dest_points_et_normal,initial,0)
-
-#icp_point_to_plane_lm(source_points,dest_points_et_normal,initial,0)
 
 
 
--- Kalmandeni/Results_robo_scans/icp_git/main.py
+++ Kalmandeni/Results_robo_scans/icp_git/main.py
@@ -1,7 +1,7 @@
 import json
 
 from Results_robo_scans.icp_example import coordinates_for_x_y__from_distance_and_angle
-from Results_robo_scans.icp_git.basicICP import icp_point_to_plane_lm, icp_point_to_plane, icp_point_to_point_lm
+from Results_robo_scans.icp_git.basicICP import icp_point_to_plane_lm, icp_point_to_plane
 import numpy as np
 import matplotlib.pyplot as plt
 from Results_robo_scans.transofmation_vector_ukf import DeniTransformation
@@ -11,21 +11,82 @@ from Results_robo_scans.transofmation_vector_ukf import DeniTransformation
 # source_points = read_file_original(fileOriginal)
 # dest_points_et_normal = read_file_deformed(deformed)
 
+def icp_point_to_point_lm(source_points, dest_points, initial, loop):
+    """
+    Point to point matching using Gauss-Newton
 
-with open("lidar_test_UKF.json",'r') as jso:
+    source_points:  nx3 matrix of n 3D points
+
+    dest_points: nx3 matrix of n 3D points, which have been obtained by some rigid deformation
+    of 'source_points'
+
+    initial: 1x6 matrix, denoting alpha, beta, gamma (the Euler angles for rotation and tx, ty, tz
+    (the translation along three axis). this is the initial estimate of the transformation
+    between 'source_points' and 'dest_points'
+
+    loop: start with zero, to keep track of the number of times it loops, just a very crude way to
+     control the recursion
+
+    """
+    J = []
+    e = []
+    for i in range(0, dest_points.shape[0] - 1):
+        dx = dest_points[i][0]
+        dy = dest_points[i][1]
+        dz = dest_points[i][2]
+        sx = source_points[i][0]
+        sy = source_points[i][1]
+        sz = source_points[i][2]
+        alpha = initial[0][0]
+        beta = initial[1][0]
+        gamma = initial[2][0]
+        tx = initial[3][0]
+        ty = initial[4][0]
+        tz = initial[5][0]
+        a1 = (-2 * beta * sx * sy) - (2 * gamma * sx * sz) + (2 * alpha * ((sy * sy) + (sz * sz))) + (
+                    2 * ((sz * dy) - (sy * dz))) + 2 * ((sy * tz) - (sz * ty))
+        a2 = (-2 * alpha * sx * sy) - (2 * gamma * sy * sz) + (2 * beta * ((sx * sx) + (sz * sz))) + (
+                    2 * ((sx * dz) - (sz * dx))) + 2 * ((sz * tx) - (sx * tz))
+        a3 = (-2 * alpha * sx * sz) - (2 * beta * sy * sz) + (2 * gamma * ((sx * sx) + (sy * sy))) + (
+                    2 * ((sy * dx) - (sx * dy))) + 2 * ((sx * ty) - (sy * tx))
+        a4 = 2 * (sx - (gamma * sy) + (beta * sz) + tx - dx)
+        a5 = 2 * (sy - (alpha * sz) + (gamma * sx) + ty - dy)
+        a6 = 2 * (sz - (beta * sx) + (alpha * sy) + tz - dz)
+        _residual = (a4 * a4 / 4) + (a5 * a5 / 4) + (a6 * a6 / 4)
+        _J = np.array([a1, a2, a3, a4, a5, a6])
+        _e = np.array([_residual])  # error
+        J.append(_J)
+        e.append(_e)
+    jacobian = np.array(J)
+    residual = np.array(e)
+    update = np.dot(np.dot(np.linalg.pinv(np.dot(np.transpose(jacobian), jacobian)), np.transpose(jacobian)), residual)
+
+    initial = initial + update
+    print(initial)
+    loop = loop + 1
+    if (loop < 1):
+        icp_point_to_point_lm(source_points, dest_points, initial, loop)
+
+
+with open("lidar_test_UKF_2.json",'r') as jso:
     testt=json.load(jso)
 
-robot_coordinates=[(0,0),(0,10),(0,30),(0,100),(90,100)]
-c=1
+
+robot_coordinates=[(0,0,0),(0,10,0),(0,30,0),(0,100,0),(0,100,90)] # (x,y,theta)
+c=3
 c_2=4
 min_=min(len(testt[f"{c}"]),len(testt[f"{c_2}"]))
 start_=0
 
 # a =np.array(testt['1'])
 # b=np.array(testt['2'])
-a=np.array([coordinates_for_x_y__from_distance_and_angle(0,robot_coordinates[c-1][0],robot_coordinates[c-1][1],ang,dist)   for ang,dist in testt[f"{c}"][start_:min_]])
-b=np.array([coordinates_for_x_y__from_distance_and_angle(0,robot_coordinates[c_2-1][0],robot_coordinates[c_2-1][1],ang,dist)   for ang,dist in testt[f"{c_2}"][start_:min_]])
+aa=[coordinates_for_x_y__from_distance_and_angle(robot_coordinates[c-1][2],robot_coordinates[c-1][0],robot_coordinates[c-1][1],ang,dist)   for ang,dist in testt[f"{c}"][start_:min_]]
+bb=[coordinates_for_x_y__from_distance_and_angle(robot_coordinates[c_2-1][2],robot_coordinates[c_2-1][0],robot_coordinates[c_2-1][1],ang,dist)   for ang,dist in testt[f"{c_2}"][start_:min_]]
 #b=np.array(dt.translocation(b,100,100,190))
+aa.append([robot_coordinates[c-1][0],robot_coordinates[c-1][1]])
+bb.append([robot_coordinates[c_2-1][0],robot_coordinates[c_2-1][1]])
+a=np.array(aa)
+b=np.array(bb)
 
 a=np.insert(a, 2, 0, axis=1) # add a Z axis=0
 
@@ -50,7 +111,9 @@ test_=Line2.copy()
 # print(Line1)
 
 #initial = np.array([[0.01], [0.05], [0.01], [0.001], [0.001], [0.001]])
-initial = np.array([[0], [0], [0], [0], [0], [0]]) # start position
+calc_dist_x= robot_coordinates[c_2 - 1][1] - robot_coordinates[c - 1][1]
+print('dddd', calc_dist_x)
+initial = np.array([[0], [0], [0], [0], [calc_dist_x], [0]]) # alpha, beta, gamma, tx, ty, tz
 
 
 
@@ -59,7 +122,7 @@ transpose=icp_point_to_plane(Line2,Line1,0)
 #print(transpose)
 print("dddddddddddddddddddddddddddddddd")
 
-#icp_point_to_point_lm(Line2,Line1,initial,0)
+icp_point_to_point_lm(Line2,Line1,initial,0)
 print("dddddddddddddddddddddddddddddddd")
 #icp_point_to_plane_lm(Line2,Line1,initial,0)
 
@@ -75,7 +138,7 @@ b=np.delete(b,2,axis=1) # remove Z axis
 
 
 DT=DeniTransformation()
-c=DT.translocation(b,0,0,0)
+c=DT.translocation(b,-3,-26,0)
 c=np.array(c)
 # x,y=data.T
 # x_t,y_t=data_transf.T
@@ -86,12 +149,15 @@ res=np.array(np.dot(test_.T,transpose))
 # res=np.delete(res,3,axis=1) # remove Z axis
 # res=np.delete(res,2,axis=1) # remove Z axis
 #print(res)
+robot=np.array(robot_coordinates)
+robot=np.delete(robot,2,axis=1) # remove Z axis
 
 plt.scatter(a[:,0], a[:,1], color=f'red', s=5,label='first scan')
 plt.scatter(b[:,0], b[:,1], color=f'green', s=5,label='second scan')
-#plt.scatter(c[:,0], c[:,1], color=f'blue', s=5,label='transformed')
+plt.scatter(c[:,0], c[:,1], color=f'blue', s=5,label='transformed')
 #plt.scatter(res[:,0], res[:,1], color=f'yellow', s=5,label='transformed_icp')
-plt.legend(loc='upper left')
+plt.plot(robot[:,0], robot[:,1], color=f'black',label='robot')
+plt.legend(loc='lower right')
 plt.grid()
 plt.show()
 
