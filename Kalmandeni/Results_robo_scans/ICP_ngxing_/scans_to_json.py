c9a63b5560b36b14b5d470b2927fc9b5cdb54dc6
Denislav <deni_9000_1990@abv.bg>
2022-10-15 11:15:27 +0200

    icp opiti 3

--- Kalmandeni/Results_robo_scans/ICP_ngxing_/scans_to_json.py
+++ Kalmandeni/Results_robo_scans/ICP_ngxing_/scans_to_json.py
@@ -26,13 +26,13 @@ counter=1
 a={}
 a['map']={}
 rc=[(0,0,0),(0,10,0),(0,30,0),(0,100,0),(0,100,90)] # (x,y,theta)
-cr=0
+cr=1
 for steps,info in testt.items():
-    if cr>1:
+    if cr>4:
         break
     data_lidar = np.array(testt[f'{counter}'])[:, 1] / 100  # in meters
     a['map'][f'{counter}']={}
-    a['map'][f'{counter}']['range'] = list(data_lidar[:200])
+    a['map'][f'{counter}']['range'] = list(data_lidar[:245])
     a['map'][f'{counter}']['theta'] =rc[counter-1][2]
     a['map'][f'{counter}']['x'] =rc[counter-1][0]/100
     a['map'][f'{counter}']['y'] =rc[counter-1][1]/100
